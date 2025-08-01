23a8cde18277db93c17f5bc73069b6fffe458896
Erick Oliveira <legrandaigle08@gmail.com>
2025-07-31 21:28:32 -0300

    Refactor code structure for improved readability and maintainability and better design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./*.{html,js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        darkgunmetal: "#192230",
+        arsenic: "#3d474e",
+        canary: "#ffed00",
+        gunmetal: "#2c2f38",
+
+        richblack: "#021A1A",
+        darkgreen: "#032221",
+        bangladesh: "#03624c",
+        meadow: "#2cc295",
+        caribbean: "#00DF81",
+        flash: "#f1f7f6",
+
+        palette1: "#021A1A",
+        palette2: "#032221",
+        palette3: "#03624c",
+        palette4: "#2cc295",
+        palette5: "#00DF81",
+        palette6: "#f1f7f6",
+      }
+    },
   },
   plugins: [],
 }
